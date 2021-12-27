dae385855aa205e75bf9544635c7d2643911937c
Ilai Segev <ilaisgv@gmail.com>
2021-12-27 21:38:57 +0200

    cleaned up constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -34,9 +34,17 @@ public final class Constants {
             public final PIDFGains steeringGains;
 
             public SwerveModuleConstants(Translation2d position, int idDrive, int idSteering) {
-                this(position, idDrive, idSteering, new PIDFGains(0.0002, 0, 0.01, 1.0/6/902.0, 50, 0), new PIDFGains(0.35, 0, 0, 0, 1, 0));
+                this(position, 
+                     idDrive, 
+                     idSteering, 
+                     new PIDFGains(0.0002, 0, 0.01, 1.0/6/902.0, 50, 0), 
+                     new PIDFGains(0.35, 0, 0, 0, 1, 0));
             }
-            public SwerveModuleConstants(Translation2d position, int idDrive, int idSteering, PIDFGains driveGains, PIDFGains steeringGains) {
+            public SwerveModuleConstants(Translation2d position, 
+                                         int idDrive, 
+                                         int idSteering, 
+                                         PIDFGains driveGains, 
+                                         PIDFGains steeringGains) {
                 this.position = position;
                 this.idDrive = idDrive;
                 this.driveGains = driveGains;
@@ -44,9 +52,15 @@ public final class Constants {
                 this.steeringGains = steeringGains;
             }
         }
-        public static final SwerveModuleConstants TLModule = new SwerveModuleConstants(new Translation2d(-0.215, 0.215), 3, 4);
-        public static final SwerveModuleConstants TRModule = new SwerveModuleConstants(new Translation2d(0.215, 0.215), 1, 2);
-        public static final SwerveModuleConstants BLModule = new SwerveModuleConstants(new Translation2d(-0.215, -0.215), 5, 6);
-        public static final SwerveModuleConstants BRModule = new SwerveModuleConstants(new Translation2d(0.215, -0.215), 7, 8);
+        
+        public static final double trackWidth = 0.43;
+        // Distance between centers of right and left wheels on robot
+        public static final double wheelBase = trackWidth;
+        // Distance between front and back wheels on robot
+
+        public static final SwerveModuleConstants TRModule = new SwerveModuleConstants(new Translation2d(wheelBase / 2, trackWidth / 2), 1, 2);
+        public static final SwerveModuleConstants BRModule = new SwerveModuleConstants(new Translation2d(wheelBase / 2, -trackWidth / 2), 7, 8);
+        public static final SwerveModuleConstants TLModule = new SwerveModuleConstants(new Translation2d(-wheelBase / 2, trackWidth / 2), 3, 4);
+        public static final SwerveModuleConstants BLModule = new SwerveModuleConstants(new Translation2d(-wheelBase / 2, -trackWidth / 2), 5, 6);
     }
 }
